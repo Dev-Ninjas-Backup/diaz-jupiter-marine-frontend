3c20d737088adbcf7d4cdb6df2fdf467606c3077
Md-Ramjan-Ali <gmramjanali8888@gmail.com>
2025-12-26 00:08:40 +0600

    complete popular category

--- src/app/(main-layout)/(home)/_components/PopularCategories/PopularCategories.tsx
+++ src/app/(main-layout)/(home)/_components/PopularCategories/PopularCategories.tsx
@@ -1,16 +1,54 @@
-import { democategoriesdata } from '@/assets/demo-datas/demodata';
+'use client';
+
 import CustomContainer from '@/components/CustomComponents/CustomContainer';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import CategoryCard from './CategoryCard';
+import { getCategories } from '@/services/category/category';
+import LoadingSpinner from '@/components/shared/LoadingSpinner/LoadingSpinner';
+import NoDataFound from '@/components/shared/NoDataFound/NoDataFound';
+
+type CategoryUI = {
+  id: string;
+  name: string;
+  image: string;
+};
 
 const PopularCategories = () => {
+  const [categories, setCategories] = useState<CategoryUI[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const loadCategories = async () => {
+      try {
+        const data = await getCategories();
+        const parsed: CategoryUI[] = data.map((item) => ({
+          id: item.id,
+          name: item.title,
+          image: item.image?.url ?? '',
+        }));
+
+        setCategories(parsed);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    loadCategories();
+  }, []);
+
+  if (loading) return <LoadingSpinner/>;
+  if(categories.length===0)return <NoDataFound dataTitle='Popular Category data'/>
+
   return (
     <CustomContainer>
       <h2 className="text-3xl sm:text-4xl lg:text-5xl text-center font-bold">
         Browse From Popular Categories
       </h2>
+
       <div className="flex flex-wrap justify-center items-center gap-10 my-10">
-        {democategoriesdata.map((category) => (
+        {categories.map((category) => (
           <CategoryCard key={category.id} category={category} />
         ))}
       </div>
--- src/services/featuredBrands/featuredBrands.ts
+++ src/services/featuredBrands/featuredBrands.ts
@@ -1,33 +1,3 @@
-// export interface FeaturedBrandResponse {
-//   id: string;
-//   site: string;
-//   featuredbrandLogo?: {
-//     url: string;
-//   };
-// }
-
-// export const getFeaturedBrands = async (site: string): Promise<FeaturedBrandResponse[]> => {
-//   try {
-//      const baseUrl = process.env.NEXT_PUBLIC_BASE_API_URL;
-//     const res = await fetch(
-//       `${baseUrl}/featured-brands?site=${site}`,
-//       {
-//         method: 'GET',
-//         next: { tags: ['FEATURED_BRANDS'] },
-//       }
-//     );
-
-//     if (!res.ok) {
-//       throw new Error(`Failed to fetch brands: ${res.status}`);
-//     }
-
-//     return res.json();
-//   } catch (error) {
-//     console.error('Featured Brands API Error:', error);
-//     throw new Error('Could not load featured brands');
-//   }
-// };
-
 export interface FeaturedBrand {
   id: string;
   site: string;
