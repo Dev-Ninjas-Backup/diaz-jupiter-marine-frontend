19e335a148136e025b6bca1b8af6096c7f9d0008
Md Habib Ullah <habibhk127@gmail.com>
2025-12-29 03:27:33 +0600

    feat; yacht details integrated

--- src/types/product-types.ts
+++ src/types/product-types.ts
@@ -8,3 +8,39 @@ export interface YachtProduct {
   built_year?: number;
   price?: number;
 }
+
+export interface BoatSpecification {
+  key: string;
+  value: string | number | boolean | null;
+}
+
+export interface BoatImage {
+  uri: string;
+}
+
+export interface BoatEngine {
+  [key: string]: string | number | boolean | null | undefined;
+}
+
+export interface BoatAdditionalInfo {
+  key: string;
+  value: string | number | boolean | null;
+}
+
+export interface BoatDetails {
+  id: string;
+  title: string;
+  price: string;
+  source: string;
+  specifications: BoatSpecification[];
+  description: string;
+  images: BoatImage[];
+  engines: BoatEngine[];
+  additionalInfo: BoatAdditionalInfo[];
+}
+
+export interface BoatDetailsResponse {
+  success: boolean;
+  message: string;
+  data: BoatDetails;
+}
