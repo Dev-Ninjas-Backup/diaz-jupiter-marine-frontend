3ae067c263b33ad0e3eac6a4acaec3da17c6eca9
Md-Ramjan-Ali <gmramjanali8888@gmail.com>
2026-01-01 02:10:28 +0600

    complete about page and contact info all api testing

--- src/app/(main-layout)/(home)/contact/_components/GetInTouch.tsx
+++ src/app/(main-layout)/(home)/contact/_components/GetInTouch.tsx
@@ -1,8 +1,5 @@
 'use client';
 
-import boatImg from '@/assets/blogs/adimg.png';
-import LoadingSpinner from '@/components/shared/LoadingSpinner/LoadingSpinner';
-import NoDataFound from '@/components/shared/NoDataFound/NoDataFound';
 import {
   ContactInfoResponse,
   getContactInfo,
@@ -12,6 +9,8 @@ import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import { FaLinkedinIn, FaTwitter, FaYoutube } from 'react-icons/fa';
 import { MdOutlineFacebook } from 'react-icons/md';
+import LoadingSpinner from '@/components/shared/LoadingSpinner/LoadingSpinner';
+import NoDataFound from '@/components/shared/NoDataFound/NoDataFound';
 
 const GetInTouch = () => {
   const [contactInfo, setContactInfo] = useState<ContactInfoResponse | null>(
@@ -68,7 +67,7 @@ const GetInTouch = () => {
   return (
     <div className="relative bg-linear-to-r from-[#006EF0] to-[#00CABE] rounded-2xl overflow-hidden">
       {/* Content Container */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-16 px-6 md:px-10 lg:px-12 py-10 md:py-12 lg:py-16">
+      <div className="relative z-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-16 px-6 md:px-10 lg:px-12 py-10 md:py-12 lg:py-16">
         {/* Get In Touch With Us */}
         <div className="text-white space-y-6">
           <h2 className="text-2xl md:text-3xl font-bold mb-6">
@@ -182,17 +181,19 @@ const GetInTouch = () => {
         </div>
 
         {/* Yacht Image */}
-        <div className="hidden max-h-60 lg:flex items-center justify-center relative">
-          <div className="relative w-full max-w-md">
-            <Image
-              src={boatImg}
-              alt="Yacht"
-              width={400}
-              height={300}
-              className="w-full h-auto object-contain drop-shadow-2xl scale-150"
-            />
+        {contactInfo.backgroundImage?.url && (
+          <div className="hidden max-h-60 lg:flex items-center justify-center relative">
+            <div className="relative w-full max-w-md">
+              <Image
+                src={contactInfo.backgroundImage.url}
+                alt="Yacht"
+                width={400}
+                height={300}
+                className="w-full h-auto object-contain drop-shadow-2xl scale-150"
+              />
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
